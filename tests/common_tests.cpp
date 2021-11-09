351e635eb356ee3b3a8f957a7db6c96ad43af32d
papazloynt <cool.chastikov53@gmail.com>
2021-11-09 22:19:59 +0300

    add tests

---
+++ tests/common_tests.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "search.h"
+}
+
+TEST(Common1, isUpper) {
+    EXPECT_TRUE(isUpper('X'));
+    EXPECT_FALSE(isUpper('c'));
+}
+
+TEST(Common2, readString) {
+    str_t str;
+
+    FILE* f = fopen("../tests/1.txt", "r");
+    EXPECT_TRUE(readString(&str, f));
+    fclose(f);
+
+    char true_result[] = "ajhkjah\"Akfdkfdokfpodf\"dfdsfsdfsdfsdf\"Aaaa\"ds";
+
+    EXPECT_EQ(str.size, 46);
+
+    for (size_t i = 0; i < str.size; ++i) {
+        EXPECT_EQ(str.seq[i], true_result[i]);
+    }
+
+    free(str.seq);
+}
